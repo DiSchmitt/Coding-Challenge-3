5340e85582c45e4fc946b0799bd671039d91c4e6
Dawson Schmitt <dischmitt@usf.edu>
2024-05-21 19:29:23 -0400

    Add brake method to decrease speed by 5 km/h.

--- Challenge3.js
+++ Challenge3.js
@@ -4,10 +4,15 @@ class Car{
         this.make = make
         this.speed = speed
     }
-    //The accelerate method that adds 10 the speed and logs it
+    //The accelerate method that adds 10 the speed and logs it.
     accelerate(){
         this.speed = this.speed + 10
         console.log(`the new speed is ${this.speed}`)
+    }
+    //The Accelerate method that decreases speed by 5 and logs it.
+    brake(){
+        this.speed = this.speed - 5
+        console.log(`the speed after braking is ${this.speed}`)
     }
 }
 //Testing the class by using the sample object.
@@ -15,3 +20,7 @@ let Car1 = new Car("BMW",120);
 console.log(Car1.make)
 console.log(Car1.speed)
 Car1.accelerate()
+Car1.brake()
+//Testing to make sure I implemented the method: it should add 10 and then decrease 5 from the new speed that the two above lines inplemented.
+Car1.accelerate()
+Car1.brake()
