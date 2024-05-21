b40dcdead84ff248da6f696e41638632be272c89
Dawson Schmitt <dischmitt@usf.edu>
2024-05-21 19:21:34 -0400

    Add accelerate method to increase speed by 10 km/h.

--- Challenge3.js
+++ Challenge3.js
@@ -4,7 +4,14 @@ class Car{
         this.make = make
         this.speed = speed
     }
+    //The accelerate method that adds 10 the speed and logs it
+    accelerate(){
+        this.speed = this.speed + 10
+        console.log(`the new speed is ${this.speed}`)
+    }
 }
-//Testing the class by making a sample object.
-let Car1 = new Car("BMW","120km/h");
+//Testing the class by using the sample object.
+let Car1 = new Car("BMW",120);
 console.log(Car1.make)
+console.log(Car1.speed)
+Car1.accelerate()
